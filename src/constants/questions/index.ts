26c2722667332bd7526915c2e4d99c4699d47330
melodoc <liubov_rz@yahoo.com>
2024-07-21 00:44:19 +0400

    add fifth topic

--- src/constants/questions/index.ts
+++ src/constants/questions/index.ts
@@ -2,3 +2,4 @@ export * from './questions1';
 export * from './questions2';
 export * from './questions3';
 export * from './questions4';
+export * from './questions5';
--- src/constants/topics.ts
+++ src/constants/topics.ts
@@ -1,11 +1,11 @@
-import { questions1, questions2, questions3, questions4 } from './questions';
+import { questions1, questions2, questions3, questions4, questions5 } from './questions';
 
 export const TOPICS_MAP = new Map([
   [1, { id: 1, name: 'Группа-1', questions: questions1 }],
   [2, { id: 2, name: 'Группа-2', questions: questions2 }],
   [3, { id: 3, name: 'Группа-3', questions: questions3 }],
   [4, { id: 4, name: 'Группа-4', questions: questions4 }],
-  [5, { id: 5, name: 'Группа-5', questions: questions1 }],
+  [5, { id: 5, name: 'Группа-5', questions: questions5 }],
   [6, { id: 6, name: 'Группа-6', questions: questions1 }],
   [7, { id: 7, name: 'Группа-7', questions: questions1 }],
   [8, { id: 8, name: 'Группа-8', questions: questions1 }],
