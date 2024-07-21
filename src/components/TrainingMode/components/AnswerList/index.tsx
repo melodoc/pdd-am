f3480c30ac2e1e657b726fa8abfe47f4eea99a2a
melodoc <liubov_rz@yahoo.com>
2024-07-22 01:18:55 +0400

    fix styles

--- src/components/TrainingMode/components/AnswerList/index.tsx
+++ src/components/TrainingMode/components/AnswerList/index.tsx
@@ -8,6 +8,11 @@ type TProps = {
     onAnswerClick: (index: number) => void;
 };
 
+const addNonBreakingSpace = (answer: string) => {
+    // Регулярное выражение для замены цифры от 1 до 10, за которой следует пробел, на цифру и неразрывный пробел
+    return answer.replace(/(\d+)\.\s/g, '$1.\u00A0');
+};
+
 const AnswerList = ({ answers, correctAnswer, selectedAnswer, showResult, onAnswerClick }: TProps) => {
     const getAnswerStyle = useCallback((index: number) => {
         if (!showResult) return 'white';
@@ -17,14 +22,30 @@ const AnswerList = ({ answers, correctAnswer, selectedAnswer, showResult, onAnsw
     }, [correctAnswer, selectedAnswer, showResult]);
 
     return (
-        <ul>
+        <ul
+            style={{
+                listStyle: "none",
+                margin: 0,
+                marginBottom: '16px',
+                padding: 0,
+            }}>
             {answers.map((answer, index) => (
                 <li
                     key={index}
                     onClick={() => onAnswerClick(index)}
-                    style={{ color: getAnswerStyle(index) }}
+                    style={{
+                        marginBottom: "8px",
+                        color: getAnswerStyle(index)
+                    }}
                 >
-                    {answer}
+                    <button type='button'
+                        style={{
+                            marginBottom: "8px",
+                            textAlign: "left",
+                            color: getAnswerStyle(index)
+                        }}>
+                        {addNonBreakingSpace(answer)}
+                    </button>
                 </li>
             ))}
         </ul>
