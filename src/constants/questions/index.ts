eb52bd767cffc058e7e257e3f9470d7fab641ed9
melodoc <liubov_rz@yahoo.com>
2024-07-21 06:11:13 +0400

    add eighth topic

--- src/constants/questions/index.ts
+++ src/constants/questions/index.ts
@@ -5,3 +5,4 @@ export * from './questions4';
 export * from './questions5';
 export * from './questions6';
 export * from './questions7';
+export * from './questions8';
--- src/constants/topics.ts
+++ src/constants/topics.ts
@@ -1,4 +1,4 @@
-import { questions1, questions2, questions3, questions4, questions5, questions6, questions7 } from './questions';
+import { questions1, questions2, questions3, questions4, questions5, questions6, questions7, questions8 } from './questions';
 
 export const TOPICS_MAP = new Map([
   [1, { id: 1, name: 'Группа-1', questions: questions1 }],
@@ -8,7 +8,7 @@ export const TOPICS_MAP = new Map([
   [5, { id: 5, name: 'Группа-5', questions: questions5 }],
   [6, { id: 6, name: 'Группа-6', questions: questions6 }],
   [7, { id: 7, name: 'Группа-7', questions: questions7 }],
-  [8, { id: 8, name: 'Группа-8', questions: questions1 }],
+  [8, { id: 8, name: 'Группа-8', questions: questions8 }],
   [9, { id: 9, name: 'Группа-9', questions: questions1 }],
   [10, { id: 10, name: 'Группа-10', questions: questions1 }]
 ]);
